275cebb19e2844dd85cdac6e0a7ccbccdb8e6455
AkshitGoel247 <137819251+AkshitGoel247@users.noreply.github.com>
2023-06-28 03:08:14 -0400

    Add files via upload
    Leetcode Question 1
    Here is the link to the question https://leetcode.com/problems/two-sum/

---
+++ LeetCode_Problems/TwoSum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int i=0; 
+        int[] ans =  {0,0};
+        while(i < nums.length){
+            int j = i+1;
+            while(j < nums.length){
+                int sum = nums[i] + nums[j];
+                if(sum != target){
+                    j++;
+                }
+                else{
+                    return new int[] {i,j};
+                }
+            }
+            i++;
+        }
+        return ans;
+    }
+}
