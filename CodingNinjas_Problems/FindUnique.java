7aabab5b34f8f23ad684abe1dffece74694a78b8
AkshitGoel247 <137819251+AkshitGoel247@users.noreply.github.com>
2023-06-29 01:08:09 -0400

    Find Unique

---
+++ CodingNinjas_Problems/FindUnique.java
@@ -0,0 +1,23 @@
+
+// Coding Ninjas problem - Find Uniquw
+//   Link to the problem - https://www.codingninjas.com/studio/problems/find-unique_625159?
+
+public class Solution{  
+
+    public static int findUnique(int[] arr){
+		//Your code goes here
+
+    // making variable to store answer
+      int ans = 0;
+
+      // looping through array 
+      for(int i=0; i<arr.length; i++){
+
+        // using XOR functionality wherein if element is XOR by itself,
+        // it returns 0 and if XOR by 0, it returns 0
+        // Hence, only element with single occurance remains in last
+         ans = ans ^ arr[i];
+      }
+      return ans;
+    }
+}
